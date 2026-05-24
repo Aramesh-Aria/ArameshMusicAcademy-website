35e298bd9704e91f451931ffb205239a9023e82c
Aria Aramesh <arya.aeamesh@gmail.com>
2026-05-24 16:10:53 +0330

    set debug false

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w(p+t0x6&*4295%5nedht10@0yz)bbdth9&0jw8xe$tnk(sgyq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aramesh-academy.runflare.run', 'api.runflare.com', 'localhost', '127.0.0.1','aramesh-academy.ir']
 
